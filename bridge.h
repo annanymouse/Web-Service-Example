b5a5c4c4bf0b7a47ee042db72ecd6b6e963fd22c
Anna Hull <annachull@gmail.com>
2015-11-24 06:37:46 -0800

    Initial Commit

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by Anna Hull on 11/24/15.
+//  Copyright © 2015 Anna Hull. All rights reserved.
+//
+#import <AFNetworking/AFNetworking.h>
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
