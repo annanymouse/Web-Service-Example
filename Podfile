a8056ceb4f11e374d62f9f2865d9d1949379a5cd
Anna Hull <annachull@gmail.com>
2015-12-05 23:28:26 -0800

    So many updates...

--- Podfile
+++ Podfile
@@ -1,3 +1,8 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '9.0'
+use_frameworks!
 pod 'AFNetworking', '~> 2.5'
+pod 'LTMorphingLabel', '~> 0.0.9'
+target 'Web Service Example' do
+    pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+end
--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -7,21 +7,46 @@
 //
 
 import UIKit
+import SwiftyJSON
+import AFNetworking
+import LTMorphingLabel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LTMorphingLabelDelegate {
     
     @IBOutlet weak var forecastLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet private var temperatureLabel: LTMorphingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET("http://api.openweathermap.org/data/2.5/forecast/daily?id=5359777&mode=json&units=imperial&cnt=1&appid=29b46f039fd2f9053f7d2c27f985f9d2",
+            parameters: nil,
+            success: { (operation:AFHTTPRequestOperation, responseObject:AnyObject) -> Void in
+                print("Response: " + responseObject.description)
+                let json = JSON(responseObject)
+                
+                if let forecast = json["list"][0]["weather"][0]["description"].string {
+                    self.forecastLabel.text = forecast }
+                if let city = json["city"]["name"].string {
+                    self.cityLabel.text = city }
+                self.temperatureLabel.morphingCharacterDelay = 9
+                let temperature = String(json["list"][0]["temp"]["day"])
+                    self.temperatureLabel.text = temperature
+                    self.temperatureLabel.morphingCharacterDelay = 0.026
+                    self.temperatureLabel.morphingDuration = 0.60
+                    self.temperatureLabel.morphingEnabled = true
+                    self.temperatureLabel.morphingProgress = 0.00
+                    self.temperatureLabel.morphingEffect = .Burn
+                })
+            {(operation:AFHTTPRequestOperation?, error:NSError) -> Void in
+                print("Error: " + error.localizedDescription)}
+        //self.temperatureLabel.morphingEffect = .Pixelate
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
